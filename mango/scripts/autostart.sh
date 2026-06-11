554df6314c0796a6dbcdbbdee3a9c910b9cdbe07
Benjy <108727379+Benjy0812@users.noreply.github.com>
2026-06-11 19:05:08 +0200

    Add DMS update .gitingnore and add DMS to auto start

--- mango/scripts/autostart.sh
+++ mango/scripts/autostart.sh
@@ -2,7 +2,7 @@
 # Autostart applications for MangoWM
 
 # Start the noctalia-shell application in the background, suppressing output
-qs -c noctalia-shell >/dev/null 2>&1 &
+dms run >/dev/null 2>&1 &
 
 # Start the swaybg application with the specified image, suppressing output
 swaybg -i ~/.config/mango/wall/1.png >/dev/null 2>&1 &

8216a9c689f83e6bb166898640939f264a087123
Benjy <108727379+Benjy0812@users.noreply.github.com>
2026-06-12 21:27:48 +0200

    yes

--- mango/scripts/autostart.sh
+++ mango/scripts/autostart.sh
@@ -2,7 +2,7 @@
 # Autostart applications for MangoWM
 
 # Start the noctalia-shell application in the background, suppressing output
-dms run >/dev/null 2>&1 &
+noctalia >/dev/null 2>&1 &
 
 # Start the swaybg application with the specified image, suppressing output
 swaybg -i ~/.config/mango/wall/1.png >/dev/null 2>&1 &
